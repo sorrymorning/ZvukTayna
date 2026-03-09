54963c24c2a86acfc9ad8fb40f3f46b05f24e258
sorrymorning <amirkazanec@gmail.com>
2026-03-09 14:22:06 +0300

    Стабильный dsss метод

--- libs/dsss.py
+++ libs/dsss.py
@@ -11,40 +11,58 @@ class Dsss(StegoMethod):
         return rng.choice([-1, 1], size=length).astype(np.float32)
 
 
-    def _mixer(L,bits,lower,upper,K):
+    def _prng(key, L):
+        out = np.ones(L, 'i4')
+        password = np.frombuffer(key.encode(), 'B')
+        max = 128 * password.size
+        seed = 1-np.sum(password)/max
 
-        from scipy.signal import windows
+        # feed seed to logistic equation to get initial value
+        x = 4 * seed * (1-seed)
+        for i in range(L - 1):
+            if x > 0.5:
+                out[i] = 1
+            else:
+                out[i] = -1
+            x = 4 * x * (1-x)
+        
+        return out
+        
 
-        if 2*K > L:
-            temp = L // 4
-            K = temp - (temp % 4)
-        else:
-            K -= K % 4
+    def _mixer(L,bits,lower=-1,upper=1,K=0):
+
+        N = bits.size
+        m_sig = np.repeat(bits, L)
+        m_sig = (m_sig * (upper - lower)) + lower
+
+        return m_sig
+    
+    def _set_power(audio_data, N, L, r, alpha):
+        powers = np.ones(N*L, 'f8')
+
+        for x in range(N):
+            power = (np.sum(audio_data[x*L : (x+1)*L] * r)) / (L*alpha)
+            if abs(power) >= 0.9:
+                powers[x*L : (x+1)*L] = abs(power) + 0.5
+        return powers
 
 
-        N = len(bits)
-        m_sig = np.repeat(bits,L)
-        hann_window = windows.hann(K)
-        c = np.convolve(m_sig,hann_window,mode='full')
-        start = K//2
-        end = len(c) - K//2 + 1
-        wnorm = c[start:end] / np.max(np.abs(c))
-        w_sig = wnorm * (upper - lower) + lower
-        m_sig = m_sig * (upper - lower) + lower
-        return w_sig,m_sig
-
-
-    def encode(self, audio_path, output_path, message,L_min=8*1024):
+    def encode(self, audio_path, output_path, message,L_min=1024):
         rate, audio = wavfile.read(audio_path)
+    
+        if audio.ndim > 1:
+            audio = audio[:, 0]
 
+        # convert to float
         if audio.dtype == np.int16:
             audio = audio.astype(np.float32) / 32768.0
-        elif audio.dtype == np.uint8:
-            audio = (audio.astype(np.float32) - 128.0) / 128.0
+
 
         bit = np.ravel([[int(y) for y in format(ord(x), '08b')] for x in message])
+
         if len(bit) == 0:
             raise ValueError("Empty message")
+        
         L2 = len(audio) // len(bit)
         L = max(L_min, L2)
         nframe = len(audio) // L
@@ -57,24 +75,20 @@ class Dsss(StegoMethod):
             padding = np.zeros(N - len(bit), dtype=int)
             bits = np.concatenate([bit, padding])
 
-        r = Dsss._gen_noise(L, 228)
+        # r = Dsss._gen_noise(L, 228)
+        r = Dsss._prng('password',L)
         pr = np.tile(r, N)
-        alpha = 0.005
+        alpha = 0.001
 
-        mix,datasig = Dsss._mixer(L,bits,-1,1,256)
-        
-
-        if audio.ndim == 1:
-            stego = audio[:N*L] + alpha * mix * pr
-            out = audio.copy()
-            out[:N*L] = stego
-        else:
-            stego = audio[:N*L, 0] + alpha * mix * pr
-            out = audio.copy()
-            out[:N*L, 0] = stego
-
-        output_int16 = (out * 32767).astype(np.int16)
-        wavfile.write(output_path, rate, output_int16)
+        mix= Dsss._mixer(L,bits)
+        stego = np.copy(audio)
+        apr = alpha * pr
+        power = Dsss._set_power(audio, N, L, r, alpha)
+        stego[:mix.size] += mix * power * apr
+        # convert back
+        output = np.clip(stego, -1, 1)
+        output = (stego * 32767).astype(np.int16)
+        wavfile.write(output_path, rate, output)
         # print(len(mix), len(pr), N*L)
         return True,f'{len(message)}'
 
@@ -82,41 +96,34 @@ class Dsss(StegoMethod):
 
 
 
-    def decode(self, audio_path,len_mes,L_min=8*1024):
+    def decode(self, audio_path,len_mes,L_min=1024):
+
         rate, audio = wavfile.read(audio_path)
 
         if audio.dtype == np.int16:
             audio = audio.astype(np.float32) / 32768.0
-        elif audio.dtype == np.uint8:
-            audio = (audio.astype(np.float32) - 128.0) / 128.0
+
 
         L2 = len(audio) // (len_mes*8)
         L = max(L_min, L2)
         nframe = len(audio) // L
         N = nframe - (nframe%8)
-
-        if audio.ndim == 1:
-            xsig = audio[:N*L].reshape((L, N), order='F')
-        else:
-            xsig = audio[:N*L, 0].reshape((L, N), order='F')
         
-        r = Dsss._gen_noise(L, 228)
+        x_sig = np.reshape(audio[:N*L], (N, L))
 
-        c = np.sum(xsig * r[:, None], axis=0) / L
+        
+        r = Dsss._prng('password',L)
 
-        # Биты
-        data_bits = np.where(c < 0, '0', '1')
-
-        # Группировка по 8 бит
-        bin_matrix = np.array(data_bits).reshape((8, N//8), order='F').T
-
-        # В символы
-        chars = [
-            chr(int(''.join(byte), 2))
-            for byte in bin_matrix
-        ]
-
-        return True,''.join(chars)[:len_mes]
+        bits = np.ones(N, dtype='int8')
+    
+        for i in range(N):
+            c = np.sum(np.multiply(x_sig[i], r))
+            if c < 0:
+                bits[i] = 0
+            else:
+                bits[i] = 1
+        chars = np.packbits(bits)
+        return True,''.join(chr(i) if i>0 and i<127 else "*" for i in chars)
 
 
 
